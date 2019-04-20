de29d8ab6bead5517c5ff0c8490d921b4462ad0a
Julia Samól <julia.samol@gmail.com>
2019-04-20 22:22:28 +0200

    Create app details view

---
+++ src/components/views/AppDetails/AppDetails.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AppDetails extends Component {
+
+  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
+
+  render() {
+
+    return (
+      <div className="animated fadeIn">
+      </div>
+    );
+  }
+}
+
+export default AppDetails;
